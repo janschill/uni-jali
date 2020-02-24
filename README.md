067b4195f6e6de48800d18db63e26bcb0c605177
Jan Schill <janschill@me.com>
2020-02-24 13:34:56 +0100

    Use node for server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # uni-project
+
+## Running node in Docker
+
+https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
---
+++ code/button/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile('/Users/janschill/code/janschill/uni-project/code/button/index.html');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
