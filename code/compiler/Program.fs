6f2f965f86fa82957bc83014b0fc58f1e98aa919
Jan Schill <janschill@me.com>
2020-02-27 23:22:39 +0100

    WIP basic compiler

--- code/compiler/Program.fs
+++ code/compiler/Program.fs
@@ -1,9 +1,20 @@
-﻿open System
+﻿module Program
+
+open System
 
 open AbstractSyntax
 open Interpreter
+open Lexer
+open Parser
+open Microsoft.FSharp.Text.Lexing
 
 [<EntryPoint>]
 let main argv =
-    eval(Prim("+", ConstantInteger 5, ConstantInteger 10))
+    printf fromString "5 + 5"
     0
+
+let fromString (str: string): expr =
+    lexbuffer = Lexing.LexBuffer<char>.FromString(str)
+    try
+        Parser.Main Lexer.Token lexbuffer
+    with exn -> printf "Error"
