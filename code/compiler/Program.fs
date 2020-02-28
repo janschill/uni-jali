bf816646d2a2500c9aeb962a98b710c59b7b7077
Jan Schill <janschill@me.com>
2020-02-28 18:04:03 +0100

    Order of compile is important

--- code/compiler/Program.fs
+++ code/compiler/Program.fs
@@ -3,19 +3,18 @@
 open System
 open FSharp.Text.Lexing
 
-open AbstractSyntax;
+open AbstractSyntax
 // open Interpreter
-open Lexer;
-open Parser;
+open Lexer
+open Parser
 
 
 let fromString (str: string): expr =
     let lexbuf = LexBuffer<char>.FromString(str)
     try
         Parser.Main Lexer.Token lexbuf
-    with 
-        | exn -> failwithf "%s\n" 
-                  (exn.Message)
+    with exn ->
+        failwithf "%s\n" (exn.Message)
 
 
 [<EntryPoint>]
