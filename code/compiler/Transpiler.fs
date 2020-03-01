4df5756f187fc08dbb2b0d60c93210c51bbb5904
Jan Schill <janschill@me.com>
2020-03-01 16:22:50 +0100

    Add missing semicolon

--- code/compiler/Transpiler.fs
+++ code/compiler/Transpiler.fs
@@ -5,7 +5,7 @@ open AbstractSyntax
 let rec eval (e: expr): string =
     match e with
     | ConstantInteger i -> string i
-    | Let(name, expression) -> sprintf "const %s = %s" name (eval expression)
+    | Let(name, expression) -> sprintf "const %s = %s;" name (eval expression)
     | Prim(operation, expression1, expression2) ->
         let value1 = eval expression1
         let value2 = eval expression2
@@ -15,3 +15,25 @@ let rec eval (e: expr): string =
         | ("*") -> sprintf "%s * %s" value1 value2
         | ("/") -> sprintf "%s / %s" value1 value2
         | ("%") -> sprintf "%s %s %s" value1 "%" value2
+(*
+const x = 1;
+let y = 1;
+
+function name(parameter) {
+    return parameter;
+}
+
+{
+    key: "value"
+}
+
+if (true) {
+
+} else if (false) {
+
+} else {
+
+}
+
+*)
+
