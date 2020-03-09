9499d8043a851b3a3838adfbd49fc4bcf79a7f4e
Jan Schill <janschill@me.com>
2020-03-09 17:08:41 +0100

    Add multi-parameter function transpiling

--- code/compiler/Transpiler.fs
+++ code/compiler/Transpiler.fs
@@ -2,44 +2,42 @@ module Transpiler
 
 open AbstractSyntax
 
-let rec eval (e: expr): string =
+let rec transpile (e: expr): string =
     match e with
-    | ConstantInteger i -> sprintf "%s" (string i)
-    | Variable v -> string v
-    | Let(name, expression) -> sprintf "const %s = %s;" name (eval expression)
-    | Prim(operation, expression1, expression2) ->
-        let value1 = eval expression1
-        let value2 = eval expression2
-        sprintf "%s %s %s" value1 operation value2
-    (*
-        match (operation) with
-        | ("+") -> sprintf "%s + %s" value1 value2
-        | ("-") -> sprintf "%s - %s" value1 value2
-        | ("*") -> sprintf "%s * %s" value1 value2
-        | ("/") -> sprintf "%s / %s" value1 value2
-        | ("%") -> sprintf "%s %s %s" value1 "%" value2
-        | (">") -> sprintf "%s > %s" value1 value2
-        | (">=") -> sprintf "%s >= %s" value1 value2
-        | ("<") -> sprintf "%s < %s" value1 value2
-        | ("<=") -> sprintf "%s <= %s" value1 value2
-        | ("==") -> sprintf "%s == %s" value1 value2
-        *)
-    | Function(name, parameter, expressions) ->
+    | Program p ->
         let rec evalExpressions es =
             match es with
-            | [ e ] -> [ sprintf " return %s" (eval e) ]
-            | e :: exs -> eval e :: evalExpressions exs
+            | [] -> ""
+            | e :: exs -> sprintf "%s%s" (transpile e) (evalExpressions exs)
+        evalExpressions p
+    | ConstantInteger i -> sprintf "%s" (string i)
+    | ConstantBoolean b -> sprintf "%s" (string b)
+    | Variable v -> string v
+    | Let(name, expression) -> sprintf "const %s = %s" name (transpile expression)
+    | Prim(operation, expression1, expression2) ->
+        let value1 = transpile expression1
+        let value2 = transpile expression2
+        sprintf "(%s %s %s)" value1 operation value2
+    | Function(name, (parameters: string list), expressions) ->
+        let parametersTranspiled = List.fold (fun acc el -> (acc + el + ", ")) "" parameters
+        let parametersTranspiledCut = (sprintf "%s" (parametersTranspiled.Remove(parametersTranspiled.Length - 2)))
+
+        let rec evalExpressions es =
+            match es with
             | [] -> []
+            | [ e ] -> [ sprintf "return %s" (transpile e) ]
+            | e :: exs -> transpile e :: evalExpressions exs
 
         let exps = evalExpressions expressions
 
         let js =
-            sprintf "function %s(%s) {\n" name parameter
+            sprintf "function %s(%s) {\n" name parametersTranspiledCut
+
         List.fold (fun s e ->
             s + sprintf "%s;\n" e) js exps
     | Tuple(expression1, expression2) ->
-        let value1 = eval expression1
-        let value2 = eval expression2
+        let value1 = transpile expression1
+        let value2 = transpile expression2
         sprintf "[%s, %s]" value1 value2
     | ADT(adtName, (constructors: adtconstructor list)) ->
         let setterGenerator count = "this.p" + string (count) + " = p" + string (count) + ";\n"
