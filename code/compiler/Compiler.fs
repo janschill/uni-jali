cf2b02bd18e0f5c2c4cf5ab4118bace0cdfb8869
Liv Hartoft Borre <liv.borre@gmail.com>
2020-04-12 12:29:07 +0200

    Cleaning

--- code/compiler/Compiler.fs
+++ code/compiler/Compiler.fs
@@ -167,13 +167,13 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
     | Constant c -> Constant c
     | Variable v ->
         match lookup v store with
-        | Dynamic e -> e // Do we really want to return e? Or do we wanna return Variable v?
+        | Dynamic e -> e // TODO: Do we really want to return e? Or do we wanna return Variable v?
         | Static vv -> Constant(vv)
     | Tuple(expr1, expr2) ->
         let rexpr1 = reduce expr1 store
         let rexpr2 = reduce expr2 store
         match (rexpr1, rexpr2) with
-        | (Constant v1, Constant v2) -> Constant(TupleValue(v1, v2)) // TODO spørg Søren, da det giver problemer i pattern matching
+        | (Constant v1, Constant v2) -> Constant(TupleValue(v1, v2))
         | _ -> Tuple(rexpr1, rexpr2)
     | Prim(operation, expression1, expression2) ->
         let rexpr1 = reduce expression1 store
@@ -203,9 +203,9 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
 
         let storeWithConstructors = List.fold (fun s c -> eval c :: s) store constructors
 
-        reduce expression storeWithConstructors // TODO IS THIS CORRECT OR SHOULD WE RETURN AN ADT???
+        reduce expression storeWithConstructors // TODO: IS THIS CORRECT OR SHOULD WE RETURN AN ADT???
     | Apply(fname, farguments) ->
-        let func = lookup fname store // This seems like a hack?
+        let func = lookup fname store
         match func with
         | Static(ADTClosure((name, argTypes), adtName, [])) ->
             let reducedArgs = List.map (fun arg -> reduce arg store) farguments
@@ -226,7 +226,7 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
                 let values = List.map (fun (Constant(c)) -> c) reducedArgs
                 Constant(ADTValue(name, adtName, values))
             else
-                Apply(fname, reducedArgs) // Can I eval here, even though i can't give eval an environment?
+                Apply(fname, reducedArgs) // TODO: Can I eval here, even though i can't give eval an environment?
 
         | Static((c)) -> Constant(c)
         | Dynamic(Function(name, parameters, expression, expression2)) ->
@@ -244,7 +244,6 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
         | _ -> failwith <| sprintf "Reduce failed on apply: %O is not a function" func
     | Pattern(matchExpression, (patternList)) ->
         let rec matchSingle (actual: Expr) (pattern: Expr) =
-            // printf "%O - %O\n\n" actual pattern
             match (actual, pattern) with
             | (_, Constant(CharValue '_')) -> Some []
             | (Constant av, Constant pv) when av = pv -> Some []
@@ -261,27 +260,11 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
                 match (matchSingle e1 p1, matchSingle e2 p2) with
                 | (Some(v1), Some(v2)) -> Some(v1 @ v2)
                 | _ -> None
-            // | (Constant(TupleValue(v1, v2)), Tuple(p1, p2)) ->
-            //     let m1 = matchSingle (Constant(v1)) p1
-            //     let m2 = matchSingle (Constant(v2)) p2
-            //     match (m1, m2) with
-            //     | (Some(v1), Some(v2)) -> Some(v1 @ v2)
-            //     | _ -> None
             | _, _ -> None
 
         let reduceExpr (expr, bindings): Expr = List.foldBack (fun b s -> b :: s) bindings store |> reduce expr
 
-        // let findSinglePattern x patterns: Expr =
-        //     let rec findPattern x patterns =
-        //         match patterns with
-        //         | (case, expr) :: tail ->
-        //             match matchSingle x case with
-        //             | None -> findPattern x tail
-        //             | Some(bindings) -> reduceExpr (expr, bindings)
-        //         | [] -> failwith "Pattern match incomplete"
-        //     findPattern x patterns
-
-        let findManyPatterns (x: Expr) patterns =
+        let findAndReduceManyPatterns (x: Expr) patterns =
             let rec findPatterns x patterns =
                 match patterns with
                 | (case, expr) :: tail ->
@@ -292,8 +275,8 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
 
             findPatterns x patterns
 
-        let findSinglePattern (v: Value) patterns =
-            match Interpreter.findPattern v patternList with
+        let evalAndReduce (v: Value) patterns =
+            match Interpreter.findPattern v patterns with
             | Some(expr, bindings) ->
                 let dsBindings = List.map (fun (name, value) -> (name, Static(value))) bindings
                 reduceExpr (expr, dsBindings)
@@ -301,19 +284,11 @@ let rec reduce (e: Expr) (store: Ds Env): Expr =
 
         let rActual = reduce matchExpression store
         match rActual with
-        | Constant c ->
-            let expr = findSinglePattern c patternList
-            // printf "FindSingle:\nmatchExpr: %O\nresult: %O\n" rActual expr
-            expr
+        | Constant c -> evalAndReduce c patternList
         | _ ->
-            let matchings = findManyPatterns rActual patternList
-            // printf "FindMany:\nmatchExpr: %O \nresult: %O\n" rActual matchings
-            match matchings with
+            match findAndReduceManyPatterns rActual patternList with
             | [] -> failwith "Pattern match incomplete"
             | [ (case, expr) ] -> expr
             | many -> Pattern(rActual, many)
 
     | _ -> failwith "No match found"
-(*
-reduce (fromString pattern) [];;
-*)
