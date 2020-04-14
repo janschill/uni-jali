5265083edb3779bf3e5f2e58712d328e38dd5b5e
Liv Hartoft Borre <liv.borre@gmail.com>
2020-04-14 19:09:39 +0200

    Add lists to pattern matching

--- code/compiler/Helpers.fs
+++ code/compiler/Helpers.fs
@@ -2,22 +2,33 @@ module Helpers
 
 open AbstractSyntax
 
+(* Reducing *)
+
 let rec matchSingleExpr (actual: Expr) (pattern: Expr) =
     match (actual, pattern) with
     | (_, Constant(CharValue '_')) -> Some []
     | (Constant av, Constant pv) when av = pv -> Some []
     | (e, Variable bindName) -> Some [ (bindName, e) ]
     | (Variable x, _) -> Some []
-    | (Apply(aConstructorName, aValues), Apply(pConstructorName, pValues)) when aConstructorName = pConstructorName ->
-        let evaluatedArguments = List.map2 matchSingleExpr aValues pValues
-        if List.forall Option.isSome evaluatedArguments
-        then Some(List.collect Option.get evaluatedArguments)
-        else None
+    | (Apply(constrName, values), Apply(patternConstrName, patternValues)) when constrName = patternConstrName ->
+        matchAllExprs values patternValues
     | (Tuple(e1, e2), Tuple(p1, p2)) ->
         match (matchSingleExpr e1 p1, matchSingleExpr e2 p2) with
         | (Some(v1), Some(v2)) -> Some(v1 @ v2)
         | _ -> None
+    | (List(exprList1), List(exprList2)) ->
+        match (exprList1, exprList2) with
+        | ([ Empt ], [ Empt ]) -> Some []
+        | _ -> matchAllExprs exprList1 exprList2
     | _, _ -> None
+
+and matchAllExprs exprs1 exprs2 =
+    let matchings = List.map2 matchSingleExpr exprs1 exprs2
+    if List.forall Option.isSome matchings
+    then Some(List.collect Option.get matchings)
+    else None
+
+(* Interpreting *)
 
 let rec matchSingleVal (actual: Value) (pattern: Expr) =
     match (actual, pattern) with
@@ -25,15 +36,22 @@ let rec matchSingleVal (actual: Value) (pattern: Expr) =
     | (a, Constant v) when a = v -> Some []
     | (a, Variable x) -> Some [ (x, a) ]
     | (ADTValue(constructorName, superName, values), Apply(callName, exprs)) when constructorName = callName ->
-        let evaluatedArguments = List.map2 matchSingleVal values exprs
-        if List.forall Option.isSome evaluatedArguments
-        then Some(List.collect Option.get evaluatedArguments)
-        else None
+        matchAllVals values exprs
     | (TupleValue(v1, v2), Tuple(p1, p2)) ->
         match (matchSingleVal v1 p1, matchSingleVal v2 p2) with
         | (Some(v1), Some(v2)) -> Some(v1 @ v2)
         | _ -> None
+    | (ListValue(valList), List(exprList)) ->
+        match (valList, exprList) with
+        | ([], [ Empt ]) -> Some []
+        | _ -> matchAllVals valList exprList
     | _, _ -> None
+
+and matchAllVals values exprs =
+    let matchings = List.map2 matchSingleVal values exprs
+    if List.forall Option.isSome matchings
+    then Some(List.collect Option.get matchings)
+    else None
 
 let rec findPattern (x: Value) patternList =
     match patternList with
--- code/compiler/Test.fs
+++ code/compiler/Test.fs
@@ -91,7 +91,14 @@ then 3
 else 4
 """
 
-let pattern = """
+let simplePattern = """
+x = 1;
+match x with
+| 1 -> true
+| _ -> false
+"""
+
+let patternInFunction = """
 func f x =
 match x with
    | Ctor1 -> 42
@@ -184,6 +191,20 @@ x = [1, 2, 3];
 x
 """
 
+let listPattern = """
+x = [1, 2, 3];
+match x with
+| [1,2,3] -> true
+| _ -> false
+"""
+
+let emptyListPattern = """
+x = [];
+match x with
+| [] -> true
+| _ -> false
+"""
+
 let testCases =
     [ minus
       string
@@ -197,14 +218,15 @@ let testCases =
       //  partialfunctionApplication;
       partialFunctionApplication2
       ifStatement
-      pattern
+      simplePattern
+      patternInFunction
       patternApplication
       complexPatternApplication
       partialComplexPatternApplication
       booleanPatternApplication
       apply
-      tuple
       adt
       adtPattern
       adtValue
       list
+      listPattern ]
