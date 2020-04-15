7486612467e0137c9f67efbaf0acd1c72e6bd7ed
Jan Schill <janschill@me.com>
2020-04-15 17:22:38 +0200

    Add assembly path to mkbundle command
    This resolves an issue when compiling all assets to dlls

--- code/compiler/src/JaLi.fsx
+++ code/compiler/src/JaLi.fsx
@@ -1,16 +1,9 @@
 exception SyntaxError of int * int
 
 [<EntryPoint>]
-let main (paramList: string []): int =
-    try
-        match paramList with
-        | [| "-i"; file |] ->
-            printfn "\n\nIIIIIII: %s\n" "0"
-        | [| "-r"; file |] ->
-            printfn "\n\nRRRRRRR: %s\n" "0"
-        | _ ->
-            printfn "\n\n----------: %s\n" "0"
-        0
-    with SyntaxError(line, col) ->
-        System.Environment.Exit 1
-        1
+let main argv =
+    printfn "Running jali"
+    match argv with
+    | [| "-i" |] -> printfn "%s" argv.[0]
+    | _ -> printfn "Invalid command"
+    0
