24f3697b2caf72750129b2e523d91d54e614ce17
Jan Schill <janschill@me.com>
2020-04-15 23:11:29 +0200

    Fix lexing of token in main

--- code/compiler/src/JaLi.fsx
+++ code/compiler/src/JaLi.fsx
@@ -1,9 +1,23 @@
+open System
+open System.Text
+open AbstractSyntax
+open FSharp.Text.Lexing
+
 exception SyntaxError of int * int
+
+let parseString (s: string): AbstractSyntax.Expr =
+    Parser.Main Lexer.Token <| LexBuffer<_>.FromBytes(Encoding.UTF8.GetBytes s)
+
+let interpret (fileName: string): Unit =
+    let program = System.IO.File.ReadAllText fileName
+    let result = Interpreter.eval (parseString program) []
+    printfn "\n\nResult for %s is: %s" fileName (AbstractSyntax.printValue result)
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Running jali"
+    printfn "Running jali …"
     match argv with
-    | [| "-i" |] -> printfn "%s" argv.[0]
+    | [| "-i"; file |] -> interpret argv.[1]
     | _ -> printfn "Invalid command"
     0
