c175f9284291058f9d91942903ee7852563e5869
Jan Schill <janschill@me.com>
2020-04-16 12:14:39 +0200

    WIP Diff and patch functions

--- code/compiler/Janitor.fs
+++ code/compiler/Janitor.fs
@@ -1,3 +1,4 @@
+[<AutoOpen>]
 module Janitor
 
 type Attribute = Att of string * string
@@ -7,6 +8,7 @@ type Node =
     | Tag of string * Attribute list * Node list
 
 type Differ =
+    | Null
     | Change of Node
     | Path of int * Differ
 
@@ -19,17 +21,32 @@ let rec equal n1 n2: bool = (n1 = n2)
 // | (Tag(t1, atts1, ns1), Tag(t2, atts2, ns2)) ->
 //     (t1=t2 && List.fold2 (fun s a1 a2 -> a1 = a2) false atts1 atts2 )
 
-let rec diff view1 view2: Option<Differ> =
-    match (view1, view2) with
-    | (Tag(t1, atts1, ns1), Tag(t2, atts2, ns2)) ->
-        let changes = fold ns1 ns2
-        match List.map Option.get changes with
-        | [] -> None
-        | cs -> Some(cs)
-    | (Text(s1), Text(s2)) ->
-        if (s1 = s2) then None else Some(Change(view2))
+let rec diff (view1: Node) (view2: Node): Differ =
+    let rec loop v1 v2 index =
+        match (v1, v2) with
+        | (Tag(t1, atts1, []), Tag(t2, atts2, [])) ->
+            if t1 = t2 && atts1 = atts2 then Null else Path(index, Change(Tag(t2, atts2, [])))
+        | (Tag(t1, atts1, ns1), Tag(t2, atts2, ns2)) ->
+            if t1 = t2 && atts1 = atts2
+            then Path(index, fold (ns1) (ns2) (index + 1))
+            else Path(index, Change(Tag(t2, atts2, [])))
 
-and fold ns1 ns2 = List.map2 (diff) ns1 ns2
+        // let changes = fold ns1 ns2
+        // match List.map Option.get changes with
+        // | [] -> None
+        // | cs -> Some(cs)
+        | (Text(s1), Text(s2)) ->
+            if (s1 = s2) then Null else Change(view2)
+    loop view1 view2 0
+
+and fold (nodes1: list<Node>) (nodes2: list<Node>) (index): Differ =
+    match (nodes1, nodes2) with
+    | (n1 :: ns1, n2 :: ns2) ->
+        if n1 = n2
+        then Path(index, fold (ns1) (ns2) (index + 1))
+        else Path(index, Change(n2))
+    | _ -> Null // this might be too naive/need more patterns
+
 // let (i, indices) =
 //     List.fold2 (fun (i, is) n1 n2 ->
 //         let diff = diff n1 n2
@@ -43,13 +60,18 @@ and fold ns1 ns2 = List.map2 (diff) ns1 ns2
   traverse view and replace nodes with nodes
   from changes by looking at index in changes
 *)
-let rec patch view changes = 0
+// this should respect the whole list, not just the head
+let rec dissectNode (node: Node): Node =
+    match node with
+    | Tag(_, _, n :: ns) -> n
 
-let view model =
-    Tag
-        ("div", [],
-         [ Text(model)
-           Text("Hello World") ])
+let rec patch (view: Node) (changes: Differ): Node =
+    match changes with
+    | Null -> view
+    | Change n -> n
+    | Path(i, d) -> patch (dissectNode view) (d)
+
+let view model = Tag("div", [], [ Text("Hello World") ])
 
 (*
 v1 = Tag ("div")
@@ -78,7 +100,7 @@ change = Some(Path(0, Change( Tag ("div" [ Tag ("button") ]) )))
 [<EntryPoint>]
 let main argv =
     let v1 = view ("1")
-    let v2 = view2 ("2")
+    let v2 = view ("2")
     let changes = diff v1 v2
     let patchedView = patch v1 changes
     0
