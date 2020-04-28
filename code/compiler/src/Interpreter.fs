6a8ff41a6f3e11710661f61bf827f65a2c3dd6f1
Liv Hartoft Borre <liv.borre@gmail.com>
2020-04-28 16:35:00 +0200

    Add concatenation on lists

--- code/compiler/src/Interpreter.fs
+++ code/compiler/src/Interpreter.fs
@@ -36,33 +36,30 @@ let rec matchSingleVal env (actual: Value) (pattern: Expr) =
         match (matchSingle v1 p1, matchSingle v2 p2) with
         | (Some(v1), Some(v2)) -> Some(v1 @ v2)
         | _ -> None
-    | (ListValue([]), List([])) -> Some []
+    // | (ListValue([]), List([])) -> Some []
     | (ListValue(valList), List(exprList)) when valList.Length = exprList.Length -> matchAllVals valList exprList
     | (ListValue(valList), ConcatC(Variable h, Variable t)) when valList.Length > 0 ->
         Some
             [ (h, List.head valList)
               (t, ListValue(List.tail valList)) ]
-    | (ListValue(valList), ConcatC(Variable h, List([]))) when valList.Length > 0 -> Some [ (h, List.head valList) ]
-    | (ListValue(valList), ConcatC(ConcatC(Variable h, Variable t), List([]))) when valList.Length > 0 ->
-        Some
-            [ (h, List.head valList)
-              (t, ListValue(List.tail valList)) ]
-    | (ListValue(valList), ConcatC(List([]), Variable t)) when valList.Length > 0 ->
-        Some [ (t, ListValue(List.tail valList)) ]
+    // | (ListValue(valList), ConcatC(Variable h, List([]))) when valList.Length > 0 -> Some [ (h, List.head valList) ]
+    // | (ListValue(valList), ConcatC(ConcatC(Variable h, Variable t), List([]))) when valList.Length > 0 ->
+    //     Some
+    //         [ (h, List.head valList)
+    //           (t, ListValue(List.tail valList)) ]
+    // | (ListValue(valList), ConcatC(List([]), Variable t)) when valList.Length > 0 ->
+    //     Some [ (t, ListValue(List.tail valList)) ]
     | _, _ -> None
 
 let rec eval (e: Expr) (env: Value Env): Value =
     match e with
     | Constant c -> c
     | Variable v -> lookup env v
-    | ConcatC(el, ls) ->
-        let evalEl = eval el env
-        match ls with
-        | List(list) -> ListValue(evalEl :: List.map (fun e -> eval e env) list)
-        | _ ->
-            ListValue
-                ([ evalEl
-                   (eval ls env) ])
+    | ConcatC(h, t) ->
+        let head = eval h env
+        match eval t env with
+        | ListValue(tail) -> ListValue(head :: tail)
+        | _ -> failwith "Evaluator failed on concatenation: tail must be a list"
     | Tuple(expr1, expr2) -> TupleValue(eval expr1 env, eval expr2 env)
     | List(list) -> ListValue(List.map (fun e -> eval e env) list)
     | And(expression1, expression2) ->
